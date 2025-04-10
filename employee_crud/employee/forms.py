bccfa27fd8e014003fe2537c863bea38ecb48656
ARES2525 <163795673+ARES2525@users.noreply.github.com>
2025-04-10 20:08:19 +0530

    Add files via upload

---
+++ employee_crud/employee/forms.py
@@ -0,0 +1,12 @@
+# employee/forms.py
+
+from django import forms
+from .models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+        widgets = {
+            'hire_date': forms.DateInput(attrs={'type': 'date'}),
+        }
